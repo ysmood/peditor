c0d0752a57e32a21707d50fd89538c11881553b4
Yad Smood <y.s.inside@gmail.com>
2013-09-02 00:32:17 +0800

    fix: A build bug.

--- kit/server_ctl.sh
+++ kit/server_ctl.sh
@@ -8,12 +8,14 @@ $coffee -o js/kit -cb kit/
 $coffee -o js/ -cb app.coffee
 $coffee -o js/lib/ -cb lib/
 $coffee -o client/js/ -cb client/lib/
+$coffee -cb client/widgets/
 
 # Compile stylus.
 if [ ! -d client/css/ ]; then
 	mkdir client/css/
 fi
 $stylus -o client/css/ client/styles/ > /dev/null
+$stylus client/widgets/*
 
 # Run controller.
 node js/kit/server_ctl.js $1
